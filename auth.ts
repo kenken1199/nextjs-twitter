127982c2c234a90320e6f61568ab1b13a55dfdf5
kenken1199 <nakamori.k@gmail.com>
2024-02-17 23:53:09 +0900

    ログイン最初完成

---
+++ auth.ts
@@ -0,0 +1,48 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+import Credentials from "next-auth/providers/credentials";
+import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
+import { User } from "@prisma/client";
+import * as bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+async function getUser(email: string): Promise<User | null> {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    return user;
+  } catch (error) {
+    console.error("Failed to fetch user:", error);
+    throw new Error("Failed to fetch user.");
+  }
+}
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(5) })
+          .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          if (!user.passwd) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.passwd);
+
+          if (passwordsMatch) return user;
+        }
+        console.log("Invalid credentials from auth.ts");
+        return null;
+      },
+    }),
+  ],
+});
